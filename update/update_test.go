2a9b888143147cacad3fd45ce070d166a7d624b6
Alex Kagno <alex@kagno.com>
2025-02-13 16:53:06 -0700

    try now!

--- update/update_test.go
+++ update/update_test.go
@@ -1,22 +1,36 @@
 package update
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"runtime"
 	"testing"
 )
 
 func TestUpdateBinary(t *testing.T) {
+	// Create test server
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("test binary content"))
 	}))
 	defer ts.Close()
 
-	AppVersion = "test"
-	AppOsArch = "test"
+	// Store original URL format
+	originalBinaryName := fmt.Sprintf("dirclean-%s-%s", runtime.GOOS, runtime.GOARCH)
 
+	// Override the binary name and URL for testing
+	binaryName = originalBinaryName
+	oldURL := fmt.Sprintf("https://github.com/arkag/dirclean/releases/download/%%s/%s", binaryName)
+
+	// Replace the URL with test server URL
+	url = ts.URL + "/" + binaryName
+
+	// Run test
 	err := UpdateBinary("test")
 	if err != nil {
 		t.Errorf("Error updating binary: %v", err)
 	}
+
+	// Restore original URL format
+	url = oldURL
 }
