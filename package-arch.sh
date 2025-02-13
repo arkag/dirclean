82588ccf9127c8d4b9aba898bbce0a86474bdac9
Alex Kagno <alex@kagno.com>
2025-02-13 16:39:15 -0700

    fix packaging

--- package-arch.sh
+++ package-arch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ ! -f /etc/arch-release ]; then
+    echo "This script must be run on an Arch Linux system"
+    exit 1
+fi
+
 VERSION=$(git describe --tags)
 makepkg -si --noconfirm
