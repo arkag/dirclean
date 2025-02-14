d5ba1b8135e8e5a0796f5729c087aff0d6fd65e4
Alex Kagno <alex@kagno.com>
2025-02-13 19:47:20 -0700

    update how uploading release assets works, remove package scripts

--- package-arch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /etc/arch-release ]; then
-    echo "This script must be run on an Arch Linux system"
-    exit 1
-fi
-
-VERSION=$(git describe --tags)
-makepkg -si --noconfirm
--- package-redhat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -f /etc/redhat-release ]; then
-    echo "This script must be run on a RedHat-based system"
-    exit 1
-fi
-
-VERSION=$(git describe --tags)
-rpmbuild -bb --define "_version ${VERSION}" dirclean.spec
